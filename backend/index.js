72530e19f1addad54b75b1abbb53d9dd29a6bc0c
fkhl1234 <fkhl1234@gmail.com>
2025-08-19 10:19:46 +0900

    250819_CORS_백+프론트연결

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,83 @@
 const express = require('express');
-const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
+const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Express 서버 시작');
+const cors = require('cors');
+app.use(cors());
+
+
+const {Sequelize, DataTypes} = require('sequelize');
+
+const sequelize = new Sequelize('codePixel', 'admin', 'admin', {
+    host: 'localhost',
+    dialect: 'mysql',
 });
 
+const user_auth = {
+    idx: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+    },
+    id: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        unique: true,
+    },
+    pw: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    }
+}
+
+sequelize.sync()
+.then(()=> console.log('DB sync success'))
+.catch(()=> console.log('DB sync fail'));
+
+
+let login = [];
+
+app.use(express.urlencoded({extended: true}));
+
 app.listen(PORT, ()=> {
-    console.log('${PORT}번 포트 개방');
+    console.log(`${PORT}번 포트 개방`);
 });
+
+app.get('/', (req, res) => {
+    res.json(login);
+});
+
+app.post('/posts', (req, res) => {
+    console.log('posts');
+    const {id, pw, name} = req.body;
+
+    login.push({idx: login.length+1, id, pw, name});
+    res.json({id, pw, name});
+});
+
+app.delete('/posts/:id', (req, res) => {
+    const id = req.params.id
+    const filteredLogin = login.filter((lg) => lg.idx !== +id);
+    const isDeleted = login.length !== filteredLogin.length;
+    login = filteredLogin;
+
+    if(isDeleted) {
+        console.log("삭제 완");
+        return;
+    }
+    console.log('삭제 안됨 ㅅㅂ');
+    return res.status(404).send('삭제 실패');
+});
+
+app.get('/user', user);
+function user(req, res) {
+    const user = url.parse(req.url, true).query;
+
+    res.json(`[user] name: ${user.name}`)
+}
+
+app.get('/api/test', (req, res)=> {
+    res.json({message: `백엔드 ${PORT} 포트 연결 완료함!`});
+})
--- frontend/src/Test.js
+++ frontend/src/Test.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { withTheme } from "@emotion/react";
 
@@ -8,6 +9,25 @@ import PersonIcon from '@mui/icons-material/Person';
 import './Test.css'
 
 export default function Test ({children}) {
+    const [msg, setMsg] = useState("");
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await fetch("http://localhost:4000/api/test");
+                if (!res.ok) throw new Error(`HTTP ${res.status}`);
+                
+                const data = await res.json();
+                setMsg(data.message);
+            } catch(error) {
+                console.error(error);
+                setMsg('Error 발생');
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <div>
             <header className="layout-header">
@@ -29,6 +49,7 @@ export default function Test ({children}) {
             <nav>
             </nav>
             <main>
+                <h1>백엔드 응답 : {msg}</h1>
                 {children}
             </main>
         </div>
